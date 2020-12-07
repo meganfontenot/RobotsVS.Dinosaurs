282eb982af6f77208f197a875bb9698c17236acf
Megan Fontenot <meganfontenot517@gmail.com>
2020-12-07 15:05:43 -0600

    instantiated MemberVariables and Constructor on the Battlefield Class.
    I have working MemberMethods drafted, but may change.

--- Battlefield.cs
+++ Battlefield.cs
@@ -9,9 +9,25 @@ namespace RobotsVS.Dinosaurs
     class Battlefield
     {
         //MemberVariables "Has A"
+        public Fleet fleet;
+        public Herd herd;
 
         //Constructor
+        public Battlefield ()
+        {
+            fleet = new Fleet();
+            herd = new Herd();
+        }
 
         //MemberMethods "Can Do"
+        public void Battle ()
+        {
+            //Need to access Dino inside of Herd and Attack() Robot inside of Fleet
+            herd.dinoHerd[0].Attack(fleet.robotFleet[0]);
+            //Need to access Robot inside of Fleet and Attack() Dino inside of Herd
+            fleet.robotFleet[0].Attack(herd.dinoHerd[0]);
+
+
+        }
     }
 }
