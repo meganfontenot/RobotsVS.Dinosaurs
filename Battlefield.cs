62f229ab17b138708e5a12fdc184231ba4613686
Megan Fontenot <meganfontenot517@gmail.com>
2020-12-07 15:25:11 -0600

    Skeleton of loop wired using CDD in Battlefield Class

--- Battlefield.cs
+++ Battlefield.cs
@@ -27,6 +27,27 @@ namespace RobotsVS.Dinosaurs
             //Need to access Robot inside of Fleet and Attack() Dino inside of Herd
             fleet.robotFleet[0].Attack(herd.dinoHerd[0]);
 
+            //Need to loop through dinos and robots for battle.
+            //When player dies (health reaches zero), player should then be removed from the list shifting herd/fleet up by one index.
+            //When one of the robot/dino list is empty (all players have died) then we can declare a winner
+            //Need to break loop if last of dino/robot dies
+
+            //dino dies
+            if (herd.dinoHerd[0].health == 0) //health reached 0
+            {
+                //remove from list
+                herd.dinoHerd.RemoveAt(0);
+            }
+
+            //robot dies
+            if (fleet.robotFleet[0].health == 0) //health reached 0
+            {
+                //remove from list
+                fleet.robotFleet.RemoveAt(0);
+            }
+
+
+
 
         }
     }
